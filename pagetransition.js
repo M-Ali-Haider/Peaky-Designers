ded6fda681a12882e25ad23938d2a1ba43c510d0
M-Ali-Haider <136558067+M-Ali-Haider@users.noreply.github.com>
2023-07-26 02:40:54 +0500

    Update pagetransition.js

--- pagetransition.js
+++ pagetransition.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const link = document.querySelector("a[href='about.html'],a[href='mainreplica.html']");
-
-    if (link) {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-
-            // Create the black screen element
-            const blackScreen = document.createElement("div");
-            blackScreen.classList.add("black-screen");
-            document.body.appendChild(blackScreen);
-
-            // Slide the black screen in
-            requestAnimationFrame(function() {
-                blackScreen.style.opacity = "1";
-                blackScreen.style.transform = "translateY(0)";
-            });
-            setTimeout(function() {
-                window.location.href = link.getAttribute("href");
-            },650);
-
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("a[href='about.html'],a[href='main.html']");
+  
+    links.forEach(link => {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+  
+        // Create the black screen element
+        const blackScreen = document.createElement("div");
+        blackScreen.classList.add("black-screen");
+        document.body.appendChild(blackScreen);
+  
+        // Slide the black screen in
+        requestAnimationFrame(function () {
+          blackScreen.style.opacity = "1";
+          blackScreen.style.transform = "translateY(0)";
         });
-    }
-});
-
+  
+        setTimeout(function () {
+          window.location.href = link.getAttribute("href");
+        }, 650);
+      });
+    });
+  });
+  
