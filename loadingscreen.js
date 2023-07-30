80604911c4052f17669143a04895014f3033e2b9
M-Ali-Haider <136558067+M-Ali-Haider@users.noreply.github.com>
2023-07-30 16:05:53 +0500

    Update loadingscreen.js

--- loadingscreen.js
+++ loadingscreen.js
@@ -16,17 +16,42 @@ function hideLoadingScreen() {
   });
 }
 
-window.addEventListener('load', function () {
+document.addEventListener('DOMContentLoaded', function () {
   var minimumLoadingTime = 2000;
 
   var loadingtext = document.querySelector('.loading-text');
   loadingtext.classList.add('fade-in');
 
   var pageLoadTime = new Date().getTime() - window.performance.timing.navigationStart;
+  var timeLeft = minimumLoadingTime - pageLoadTime;
 
-  if (pageLoadTime < minimumLoadingTime) {
-    setTimeout(hideLoadingScreen, minimumLoadingTime - pageLoadTime);
-  } else {
-    hideLoadingScreen();
+  // Calculate the number of resources to be loaded (e.g., images, stylesheets)
+  var resourceCount = 0;
+  var loadedResources = 0;
+  var resources = document.querySelectorAll('img, link[rel="stylesheet"]');
+
+  function resourceLoaded() {
+    loadedResources++;
+    if (loadedResources === resourceCount && timeLeft <= 0) {
+      hideLoadingScreen();
+    }
   }
+
+  resourceCount = resources.length;
+
+  // Check if resources are already loaded (cached) before adding the event listeners
+  for (var i = 0; i < resourceCount; i++) {
+    if (resources[i].complete) {
+      resourceLoaded();
+    }
+  }
+
+  // Add event listeners for resources that are not yet loaded
+  for (var i = 0; i < resourceCount; i++) {
+    resources[i].addEventListener('load', resourceLoaded);
+    resources[i].addEventListener('error', resourceLoaded); // Handle resource loading errors too
+  }
+
+  // Handle the case where the minimumLoadingTime is not reached, but all resources are loaded
+  setTimeout(hideLoadingScreen, Math.max(timeLeft, 0));
 });
