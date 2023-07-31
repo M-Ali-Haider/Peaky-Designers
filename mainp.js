eba21c6a767d49930104204104f3885cfb0745da
M-Ali-Haider <136558067+M-Ali-Haider@users.noreply.github.com>
2023-07-31 19:25:17 +0500

    Update mainp.js

--- mainp.js
+++ mainp.js
@@ -1,17 +1,3 @@
-let sky = document.getElementById('sky');
-let back = document.getElementById('back-m');
-let front = document.getElementById('front-mount');
-let birbs = document.getElementById('birbs');
-let explore = document.getElementById('Explore');
-
-window.addEventListener('scroll',function () {
-    let value = window.scrollY;
-    back.style.top = value * 0.45 + 'px';
-    birbs.style.left =- value * 0.5 + 'px';
-    sky.style.top = value * 0.55 + 'px';
-    //explore.style.marginTop = value * 0.6 + 'px';
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     // Function to handle the scroll event
     function handleScroll() {
