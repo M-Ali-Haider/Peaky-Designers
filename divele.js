496713143ae4e3e856770f0850b7bb4d227a770b
Abdullah-Khalil123 <106603093+Abdullah-Khalil123@users.noreply.github.com>
2023-08-10 16:35:44 +0500

    Update divele.js

--- divele.js
+++ divele.js
@@ -5,13 +5,37 @@ const grid = document.getElementsByClassName('grid5');
 const line = document.getElementById('line');
 const icon = document.getElementById('icon');
 const line2 = document.getElementById('line2');
+const rad_grad = document.getElementById('rad-grad');
+const smoltabs = document.getElementsByClassName('smallbox');
+const smolgrad1 = document.getElementById('smolgrad1');
+const smolgrad2 = document.getElementById('smolgrad2');
 
 line.style.marginLeft = 70 + tab.offsetLeft + 'px';
 icon.style.marginLeft = 70 + tab.offsetLeft + 'px';
 line2.style.marginLeft = 70 + tab.offsetLeft + 'px';
 
+rad_grad.style.left = 70 + tab.offsetLeft + 'px';
+rad_grad.style.top = 0 + 'px';
 
+smoltabs[0].addEventListener('mousemove',function(e){
+    smolgrad1.classList.add('smalgradshow');
+    smolgrad1.style.transform = `translate(${e.pageX - smolgrad1.clientWidth / 2 - smoltabs[0].offsetLeft}px,${e.pageY - window.scrollY - smoltabs[0].getBoundingClientRect().top - smoltabs[0].clientHeight/2}px)`;
+});
+smoltabs[1].addEventListener('mousemove',function(e){
 
+    smolgrad2.classList.add('smalgradshow');
+    smolgrad2.style.transform = `translate(${e.pageX - smolgrad2.clientWidth / 2 - smoltabs[1].offsetLeft}px,${e.pageY - window.scrollY - smoltabs[1].getBoundingClientRect().top - smoltabs[1].clientHeight/2}px)`;
+
+});
+smoltabs[0].addEventListener('mouseleave',function(){
+
+    smolgrad1.classList.remove('smalgradshow');
+
+});
+smoltabs[1].addEventListener('mouseleave',function(){
+    smolgrad2.classList.remove('smalgradshow');
+
+});
 var max_rot = 3;
 tab.addEventListener('mousemove',function(e){
     var pagex = e.pageX - tab.offsetLeft;
@@ -34,7 +58,6 @@ tab.addEventListener('mouseleave',function(){
 const options = {
     root: null,
     threshold: 0,
-    rootMargin: "-90px",
 };
 
 const obs = new IntersectionObserver(function(entries,observer){
@@ -60,11 +83,15 @@ const obs = new IntersectionObserver(function(entries,observer){
 const obs2 = new IntersectionObserver(function(entries,observer){
     entries.forEach(entry => {
         if(entry.isIntersecting){
+
             line2.classList.add('lineshow');
+            rad_grad.style.opacity = 1;
+
+
         }
         else{
             line2.classList.remove('lineshow');
-
+            rad_grad.style.opacity = 0;
         }
     });
 },);
