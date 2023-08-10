19f70c54559a7cae7380780e416ac21d7c7f59f9
M-Ali-Haider <136558067+M-Ali-Haider@users.noreply.github.com>
2023-08-11 02:43:33 +0500

    Update menubutton.js

--- menubutton.js
+++ menubutton.js
@@ -1,4 +1,14 @@
-const magicButton = document.querySelector(".magic-button");
+const magicButton = document.querySelector('.magic-button');
+const topBar = document.querySelector('.top-bar');
+const bottomBar = document.querySelector('.bottom-bar');
+const whymagic =document.querySelector('.why-magic');
+const sideButtons = document.querySelectorAll('.sideButton');
+const magicButton4=document.querySelector('.jdcr');
+const whyMagic=document.querySelector('.jdcrt');
+const bigcircle=document.querySelector('.majin')
+const bigcircleinside=document.querySelector('.lilmajin');
+let rotated = false;
+
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 1) {
     magicButton.style.display = "block";
@@ -11,125 +21,97 @@ window.addEventListener("scroll", () => {
   }
 });
 
+magicButton.addEventListener('click',()=>{
+  rotated=!rotated;
+  whymagic.style.gap=rotated?'0px':'6.5px';
+  topBar.style.transform=rotated?'rotate(-45deg)':'rotate(0)';
+  bottomBar.style.transform=rotated?'rotate(45deg)':'rotate(0)';
+  magicButton.style.backgroundColor=rotated?'white':'#1d1e20';
+  bottomBar.style.backgroundColor=rotated?'#1d1e20':'gray';
+})
 
-const magicButton2 = document.querySelector('.magic-button');
-const topBar = document.querySelector('.top-bar');
-const bottomBar = document.querySelector('.bottom-bar');
-const whymagic =document.querySelector('.why-magic');
-let rotated = false;
-
-magicButton2.addEventListener('click', () => {
-  if (!rotated) {
-    document.body.style.overflow = 'hidden';
-    whymagic.style.gap=`0px`;
-    topBar.style.transform = 'rotate(-45deg)';
-    topBar.style.backgroundColor=`white`;
-    bottomBar.style.transform = 'rotate(45deg)';
-    magicButton2.style.backgroundColor=`white`;
-    topBar.style.backgroundColor=`#1d1e20`;
-    bottomBar.style.backgroundColor=`#1d1e20`;
-
-  } else {
-    document.body.style.overflow = 'auto';
-    document.body.classList.remove('shaded-background');
-    whymagic.style.gap=`6.5px`;
-    bottomBar.style.backgroundColor=`gray`;
-    topBar.style.transform = 'none';
-    bottomBar.style.transform = 'none';
-    magicButton2.style.backgroundColor=`#1d1e20`;
-    bottomBar.style.backgroundColor=`gray`;
-  }
-
-  rotated = !rotated;
-});
-
-
-magicButton2.addEventListener('mouseenter',()=>{
+magicButton.addEventListener('mouseenter',()=>{
   topBar.style.backgroundColor=`#1d1e20`;
 });
+
 magicButton.addEventListener(`mouseleave`,()=>{
   if(!rotated){
     topBar.style.backgroundColor=`white`;
   }
 });
-    const sideButtons = document.querySelectorAll('.sideButton');
-    sideButtons.forEach(somethingHelper => {
-      somethingHelper.addEventListener('mousemove', (e) => {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
+
+
+sideButtons.forEach(somethingHelper => {
+somethingHelper.addEventListener('mousemove', (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  const helperRect = somethingHelper.getBoundingClientRect();
+  const helperCenterX = helperRect.left + helperRect.width / 10;
+  const helperCenterY = helperRect.top + helperRect.height /10;
+  const offsetX = (mouseX - helperCenterX) / 5;
+  const offsetY = (mouseY - helperCenterY) / 5;
+  somethingHelper.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+});
+  somethingHelper.addEventListener('mouseleave', () => {
+    somethingHelper.style.transform = 'translate(0, 0)';
+  });
+});
+
+magicButton4.addEventListener('mousemove',(e)=>{
+    const mouseXs = e.clientX;
+    const mouseYs = e.clientY;
+    const magicRect = magicButton4.getBoundingClientRect();
+    const magicCenterX = magicRect.left + magicRect.width /2;
+    const magicCenterY = magicRect.top + magicRect.height /2;
+    const offsetXs = (mouseXs - magicCenterX) /2;
+    const offsetYs = (mouseYs - magicCenterY) /2;
+
+    const offX=(mouseXs - magicCenterX) /3;
+    const offY = (mouseYs - magicCenterY) /3;
+
+    magicButton4.style.transform = `translate(${offsetXs}px, ${offsetYs}px)`;
+    whyMagic.style.transform = `translate(${offX}px, ${offY}px)`;
+  });
+  magicButton4.addEventListener('mouseleave', () => {
+    magicButton4.style.transform = 'translate(0, 0)';
+    whyMagic.style.transform = 'translate(0, 0)';
+  });    
+
+bigcircle.addEventListener('mousemove',(e)=>{
+    const mouseXs = e.clientX;
+    const mouseYs = e.clientY;
+    const magicRect = bigcircle.getBoundingClientRect();
+    const magicCenterX = magicRect.left + magicRect.width /2;
+    const magicCenterY = magicRect.top + magicRect.height /2;
+    const offsetXs = (mouseXs - magicCenterX) /3.05;
+    const offsetYs = (mouseYs - magicCenterY) /3.05;
+    const offseterX = (mouseXs - magicCenterX) /6;
+    const offseterY = (mouseYs - magicCenterY) /6;
+    bigcircle.style.transform = `translate(${offsetXs}px, ${offsetYs}px)`;
+    bigcircleinside.style.transform = `translate(${offseterX}px, ${offseterY}px)`;
+  });
+  bigcircle.addEventListener('mouseleave', () => {
+    bigcircle.style.transform = 'translate(0, 0)';
+    bigcircleinside.style.transform = 'translate(0, 0)';
+  });    
   
-        const helperRect = somethingHelper.getBoundingClientRect();
-        const helperCenterX = helperRect.left + helperRect.width / 10;
-        const helperCenterY = helperRect.top + helperRect.height /10;
   
-        const offsetX = (mouseX - helperCenterX) / 5;
-        const offsetY = (mouseY - helperCenterY) / 5;
+
+
+
+
+
+
+
   
-        somethingHelper.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-      });
+        
   
-      somethingHelper.addEventListener('mouseleave', () => {
-        somethingHelper.style.transform = 'translate(0, 0)';
-      });
-    });
-
-
-    const magicButton4 = document.getElementById('magicButton');
-    const whyMagic = document.getElementById('whymagicc');
-
-    const range=3.75;
-    const ranger=4.75;
-
-    magicButton4.addEventListener('mousemove', (e) => {
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
-
-      const magicRect = magicButton4.getBoundingClientRect();
-      const magicCenterX = magicRect.left + magicRect.width /2;
-      const magicCenterY = magicRect.top + magicRect.height /2;
-
-      const offsetX = (mouseX - magicCenterX) / range;
-      const offsetY = (mouseY - magicCenterY) / range;
-
-      const offseterX = (mouseX - magicCenterX) / ranger;
-      const offseterY = (mouseY - magicCenterY) / ranger;
-
-
-      magicButton4.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-      whyMagic.style.transform = `translate(${offseterX}px, ${offseterY}px)`;
-    });
-
-    magicButton.addEventListener('mouseleave', () => {
-      magicButton4.style.transform = 'translate(0, 0)';
-      whyMagic.style.transform = 'translate(0, 0)';
-    });
-
-
-
-    const bbt = document.getElementById('bbt');
-    const bbtj = document.getElementById('bbtj');
+        
+  
+        
+  
+      
     
-    bbt.addEventListener('mousemove', (e) => {
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
-      const bbtRect = bbt.getBoundingClientRect();
-      const bbtCenterX = bbtRect.left + bbtRect.width /2;
-      const bbtCenterY = bbtRect.top + bbtRect.height /2;
 
-      const offsetX = (mouseX - bbtCenterX) / range;
-      const offsetY = (mouseY - bbtCenterY) / range;
-
-      const offseterX = (mouseX - bbtCenterX) / ranger;
-      const offseterY = (mouseY - bbtCenterY) / ranger;
-
-
-      bbt.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-      bbtj.style.transform = `translate(${offseterX}px, ${offseterY}px)`;
-    });
-
-    bbt.addEventListener('mouseleave', () => {
-      bbt.style.transform = 'translate(0, 0)';
-      bbtj.style.transform = 'translate(0, 0)';
-    });
-  
+    
     
